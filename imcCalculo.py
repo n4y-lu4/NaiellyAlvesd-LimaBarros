67057bb9bc0907459232a0c0b2867668ff815cc2
Naielly Barros <naielly62364086@alunos.sc.senac.br>
2026-03-10 21:24:04 -0300

    imc-calculadora

---
+++ imcCalculo.py
@@ -0,0 +1,15 @@
+#imc 
+nome =input("nome:")
+peso = float(input("peso:"))
+altura = float(input("altura:"))
+
+imc= peso / (altura **2)
+print(f"imc de {nome}: {imc:.2f}" )
+
+baixoPeso = imc <18.5
+normal = (imc >= 18.5) and (imc <25)
+sobrePeso = (imc>=30)
+
+print("baixo peso ?",baixoPeso)
+print("normal?",normal)
+print("obesidade?",sobrePeso)
