f38c0f73831a4a25914da7c5ed1ab282d5a76da9
𝔫𝔞𝔦𝔢́𝔩𝔩𝔶 𝔞𝔩𝔳𝔢𝔰 𝔡´𝔩𝔦𝔪𝔞 𝔟𝔞𝔯𝔯𝔬𝔰 <bobagemm123@gmail.com>
2026-04-07 19:11:58 -0300

    Add files via upload

---
+++ Laço_repetição/lacorepeticao.py
@@ -0,0 +1,19 @@
+valor = 1
+total = 0
+quantidade = 0
+
+valor = float(input("valor da comprq:"))
+
+while valor != 0:
+    valor = float(input("valor da compra:"))
+
+    total +=valor
+    quantidade !=1
+
+
+    #if quantidade>0:
+    print(f'total:rs {total:.2f} , media {total:2f}')
+
+    #else :
+     #   print("nehum valor")
+
